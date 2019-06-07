71507787b321f4b4a23f105216cfb633b66275f7
Daniel Dias <danieldias15@gmail.com>
2019-06-07 05:22:03 -0400

    Prototype is as done as it'll get for now

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -13,58 +13,34 @@ public class ThirdPersonCamera : MonoBehaviour {
     public float maxIn;
     public float maxOut;
 
-    public float correctiveForce;
-    public float dampeningForce;
+    public float cameraMovementScalar;
 
     private PrototypePlayerController playerController;
 
     void Start() {
         camera = gameObject.GetComponent<Camera>();
         playerController = player.GetComponent<PrototypePlayerController>();
-    }
-
-    public Vector3 cameraCorrectionVelocity = new Vector3(0f, 0f, 0f);
-    public Vector3 previousPosition;
-
-    void LateUpdate() {
-        Vector3 desiredLocation = player.transform.position + player.transform.rotation * (localPositionPivot + offset);
-        Vector3 direction = transform.position - desiredLocation;
-
-        Vector3 test = Quaternion.Inverse(player.transform.rotation) * direction;
-        Debug.Log(test.z);
-
-        if (test.z > maxIn) {
-            transform.position += player.transform.rotation * ((test.z - maxIn) * -Vector3.forward);
-            direction = transform.position - desiredLocation;
-        }
-        else if (test.z < -maxOut) {
-            transform.position += player.transform.rotation * ((test.z - (-maxOut)) * -Vector3.forward);
-            direction = transform.position - desiredLocation;
-        }
-
-        Vector3 cameraMovementCorrection;
-        // F = ma = m * v/t = -k * x^2 - b * v, but only apply this along the camera's forward vector.
-        if (Time.deltaTime > 0)
-            cameraMovementCorrection = -correctiveForce * direction + dampeningForce * (previousPosition - transform.position) / Time.deltaTime;
-        else
-            cameraMovementCorrection = -correctiveForce * direction;
-        Vector3 springForce = -correctiveForce * Mathf.Pow(direction.magnitude, 2) * direction;
-
-        Vector3 damperVec = Vector3.zero;
-        if (Time.deltaTime > 0)
-            damperVec = dampeningForce * (previousPosition - transform.position) / Time.deltaTime;
-
-        Debug.Log(previousPosition - transform.position);
-        Debug.Log("Spring = (" + springForce.x + ", " + springForce.y + ", " + springForce.z + ") - Damper = (" + damperVec.x + ", " + damperVec.y + ", " + damperVec.z + ")");
-
-        cameraCorrectionVelocity += cameraMovementCorrection * Time.deltaTime;
 
         previousPosition = transform.position;
+        previousRotation = playerController.AimDirection();
+    }
 
-        transform.position += cameraMovementCorrection * Time.deltaTime * Time.deltaTime; // cameraCorrectionVelocity * Time.deltaTime;
-        transform.rotation = playerController.AimDirection() * Quaternion.Euler(12f, 0f, 0f);
+    public Vector3 previousPosition;
+    public Quaternion previousRotation;
+    public Quaternion relativeRotation;
 
+    void LateUpdate() {
+        Quaternion cameraRotation = playerController.AimDirection();
+        Vector3 desiredLocation = player.transform.position + cameraRotation * offset;
         Vector3 cameraAlignProjection = Vector3.Project(transform.position - desiredLocation, playerController.AimDirection() * -Vector3.forward);
-        transform.position = desiredLocation + cameraAlignProjection;
+
+            // Doesn't work, I'll have to break apart cameraRotation to two separate quaternions.
+        Vector3 cameraDirection = desiredLocation - cameraRotation * localPositionPivot;
+
+        previousPosition = transform.position;
+        previousRotation = transform.rotation;
+
+        transform.rotation = playerController.AimDirection() * Quaternion.Euler(12f, 0f, 0f);
+        transform.position = desiredLocation; // + cameraAlignProjection;
     }
 }
