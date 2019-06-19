cf667dbb348a02b9e4f4893c4d87c0dfd22638dd
Daniel Dias <danieldias15@gmail.com>
2019-06-19 11:48:22 -0400

    Added PlayerController as a prefab

---
+++ Assets/Scripts/MecanimBehaviours/EventTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTimer : StateMachineBehaviour {
+
+    public UnityEvent unityEvent;
+    public UnityEvent[] unityEvents;
+
+    public float time = 0f;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) {
+        time = 0f;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) {
+        time = 10f;
+    }
+}
