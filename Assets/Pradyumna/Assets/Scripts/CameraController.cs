9c4fbcc05280970115a60f5cc27fa49cfef1a733
Pradyumna Mukunda <pmukunda3@gatech.edu>
2019-06-26 11:18:30 -0400

    Added weapons and effects

---
+++ Assets/Pradyumna/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //public float Scroll_Sensitivity = 1f;
+
+    public float MaxInclination = 90f;
+    public float MinInclination = -90f;
+    public float CameraDistance = 7f;
+    
+    private Vector3 NewPosition, Offset;
+    private Quaternion NewRotation;
+    private float MouseYPos, Lat;
+
+    void Start()
+    {
+        transform.localScale = CameraDistance * Vector3.one;
+        transform.localPosition = new Vector3(0, CameraDistance/4, 0);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        MouseYPos = Input.mousePosition.y / Screen.height;
+        Lat = Mathf.Lerp(MaxInclination, MinInclination, MouseYPos);
+        transform.localEulerAngles = new Vector3(Lat, 0, 0);
+    }
+}
---
+++ Assets/Pradyumna/Assets/Scripts/ProjectileWeaponController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileWeaponController : MonoBehaviour
+{
+    public GameObject Muzzle;
+    public GameObject Projectile;
+    public GameObject FirePoint;
+    public float MuzzleDuration;
+    public float FireRate;
+
+    private float clock;
+    private float FireInterval;
+    private GameObject NewProjectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = 0f;
+        FireInterval = 1 / FireRate;
+        Projectile.SetActive(false);
+        Projectile.GetComponent<ProjectileController>().dontDestroy = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clock += Time.deltaTime;
+        if (clock > MuzzleDuration)
+        {
+            Muzzle.SetActive(false);
+        }
+        if (clock > FireInterval)
+        {
+            Muzzle.SetActive(true);
+            clock = 0f;
+            NewProjectile = Instantiate(Projectile, FirePoint.transform);
+            NewProjectile.transform.parent = null;
+            NewProjectile.SetActive(true);
+            NewProjectile.GetComponent<ProjectileController>().dontDestroy = false;
+        }
+    }
+}
