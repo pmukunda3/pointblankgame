507155d671685c8fdaf42ee065c7931631beb4d8
Daniel Dias <danieldias15@gmail.com>
2019-07-01 11:52:06 -0400

    Moved UI code, deleted prototype char

---
+++ Assets/Scripts/UI/GameStarter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class GameStarter : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+}
