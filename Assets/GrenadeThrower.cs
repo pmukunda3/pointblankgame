705848311c992c5c6dde29432c8929284d0b3f61
Pradyumna Mukunda <pmukunda3@gatech.edu>
2019-07-06 03:02:35 -0400

    Added grenade

---
+++ Assets/GrenadeThrower.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour
+{
+    public GameObject grenade;
+    public float throwVelocity;
+    private GameObject newGrenade;
+
+    private Rigidbody rb;
+
+    void Throw()
+    {
+        newGrenade = Instantiate(grenade, transform);
+        newGrenade.transform.parent = null;
+        newGrenade.GetComponent<Grenade>().explode = true;
+        newGrenade.GetComponent<Collider>().enabled = true;
+        rb = newGrenade.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.AddRelativeForce(new Vector3(0,0,throwVelocity),ForceMode.VelocityChange);       
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grenade.GetComponent<Collider>().enabled = false;
+        grenade.GetComponent<Rigidbody>().isKinematic = true;
+        grenade.GetComponent<Grenade>().explode = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Throw();
+        }
+    }
+}
--- Assets/Scripts/WeaponControl/ContinuousWeaponController.cs
+++ Assets/Scripts/WeaponControl/ContinuousWeaponController.cs
@@ -6,7 +6,7 @@ public class ContinuousWeaponController : MonoBehaviour
 {
     public GameObject beam;
 
-    void Start()
+    void OnEnable()
     {
         beam.SetActive(false);
     }
