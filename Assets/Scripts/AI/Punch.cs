2141eda81b27238986761d496979ff79c8e8eaf4
kedi <zhengkedi27@gmail.com>
2019-07-17 12:07:41 -0400

    Punch working

--- Assets/Scripts/AI/Punch.cs
+++ Assets/Scripts/AI/Punch.cs
@@ -8,10 +8,17 @@ public class Punch : MonoBehaviour
 
     void OnTriggerEnter(Collider hit_obj)
     {
-        if(hit_obj.tag == "Player")
+        Debug.Log("Hitting something");
+        Debug.Log(hit_obj);
+        if (hit_obj.gameObject.layer == LayerMask.NameToLayer("Player Character"))
         {
             Debug.Log("It hit the player");
         }
 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exiting the pubch collider");
     }
 }
