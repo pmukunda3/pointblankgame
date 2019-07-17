0b49df9869a539ec559d9852a3d9b665dc69ac86
Daniel Dias <danieldias15@gmail.com>
2019-07-17 11:42:34 -0400

    Updated Prefabs and game code

--- Assets/Scripts/Character/Game.cs
+++ Assets/Scripts/Character/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace PlayerControl {
     public class Game : MonoBehaviour {
@@ -8,6 +9,21 @@ namespace PlayerControl {
         public AnimationCurve fallDamageCurve;
 
         private int healthPoints = 100;
+        private UnityAction<int> damageAction;
+        private UnityAction<float> fallDamageAction;
+
+        public void Start() {
+            damageAction = new UnityAction<int>(TakeDamage);
+            fallDamageAction = new UnityAction<float>(FallDamage);
+
+            EventManager.StartListening<PlayerDamageEvent, int>(damageAction);
+            EventManager.StartListening<PlayerFallDamageEvent, float>(fallDamageAction);
+        }
+
+        public void OnDestroy() {
+            EventManager.StopListening<PlayerDamageEvent, int>(damageAction);
+            EventManager.StopListening<PlayerFallDamageEvent, float>(fallDamageAction);
+        }
 
         public void FallDamage(float verticalVelocity) {
             int damage = Mathf.RoundToInt(fallDamageCurve.Evaluate(verticalVelocity));
@@ -19,5 +35,17 @@ namespace PlayerControl {
                 healthPoints -= damage;
             }
         }
+
+        public void TakeDamage(int amount) {
+            healthPoints -= amount;
+
+            if (healthPoints <= 0) {
+                EventManager.TriggerEvent<PlayerDeathEvent>();
+            }
+        }
     }
+
+    public class PlayerDamageEvent : UnityEvent<int> { }
+    public class PlayerFallDamageEvent : UnityEvent<float> { }
+    public class PlayerDeathEvent : UnityEvent { }
 }
