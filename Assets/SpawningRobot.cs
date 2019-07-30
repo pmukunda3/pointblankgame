63c0cb8708e392198bcd1936fb0243139b32adb8
Ankit Arora <anki@gatech.edu>
2019-07-30 15:49:25 -0400

    Fixed robot spawn prefabs

---
+++ Assets/SpawningRobot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningRobot : MonoBehaviour
+{
+    public GameObject ninjaPrefab;
+    public BoxCollider col;
+    private Transform[] SpawnPoints;
+
+
+
+
+    private void Start()
+    {
+        col = gameObject.GetComponent<BoxCollider>();
+        Transform[] temp = gameObject.GetComponentsInChildren<Transform>();
+        //this.SpawnPoints = temp;
+        this.SpawnPoints = new Transform[temp.Length - 1];
+        for(int i = 0; i < this.SpawnPoints.Length; ++i)
+        {
+            this.SpawnPoints[i] = temp[i + 1];
+        }
+
+    }
+    private void SpawnNinja(Vector3 position)
+    {
+        Debug.Log("spawning");
+        GameObject a = Instantiate(ninjaPrefab) as GameObject;
+        a.transform.position = position;  
+        a.transform.localPosition = position;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player Character"))
+        {
+            Debug.Log("Player enter");
+            foreach (Transform t in this.SpawnPoints)
+            {
+                SpawnNinja(t.position);
+            }
+            col.enabled = false;
+        }
+    }
+}
