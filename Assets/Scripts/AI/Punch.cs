efcad3e6b5db65912f067969f121c9419565ce07
Daniel Dias <danieldias15@gmail.com>
2019-07-31 05:05:29 -0400

    Put all code files back where they should be

---
+++ Assets/Scripts/AI/Punch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PlayerControl;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+    private bool hitAlready = false;
+    private Animator ai_animator;
+
+    private void Start()
+    {
+        ai_animator = gameObject.GetComponentInParent<Animator>();
+
+    }
+    void OnTriggerEnter(Collider hit_obj)
+    {
+
+        if (hit_obj.gameObject.layer == LayerMask.NameToLayer("Player Character")&&
+        ai_animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            if (!hitAlready)
+            {
+                Debug.Log("It hit the player");
+                EventManager.TriggerEvent<PlayerDamageEvent, int>(1);
+                hitAlready = true;
+            }
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exiting the pubch collider");
+        hitAlready = false;
+    }
+}
