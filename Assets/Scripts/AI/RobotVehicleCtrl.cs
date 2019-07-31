5d6f3995a37feea294028eb18b260440ad3afe74
Ankit Arora <anki@gatech.edu>
2019-07-31 00:02:17 -0400

    Bug fixes

--- Assets/Scripts/AI/RobotVehicleCtrl.cs
+++ Assets/Scripts/AI/RobotVehicleCtrl.cs
@@ -38,7 +38,7 @@ public class RobotVehicleCtrl : MonoBehaviour
         trigger.SetCallback(new UnityAction(StartAnimating));
         VhState = VehicleState.Idle;
         rg = gameObject.GetComponent<Rigidbody>();
-        VhState = VehicleState.GoToDrop;
+        //VhState = VehicleState.GoToDrop;
     }
 
     private void StartAnimating()
