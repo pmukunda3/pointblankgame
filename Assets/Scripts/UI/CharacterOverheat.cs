a14fe0b0a6f264835b24a822fbc19b2dea518ff1
Pradyumna Mukunda <pmukunda3@gatech.edu>
2019-07-30 21:39:26 -0400

    Weapon no longer floats in midair on player death

---
+++ Assets/Scripts/UI/CharacterOverheat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterOverheat : MonoBehaviour
+{
+    public float CurrHeat { get; set; }
+    public float MaxHeat { get; set; }
+
+    public Slider overheatBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrHeat = 0;
+        overheatBar.value = CalculateOverheat();
+    }
+
+    void Update()
+    {
+        overheatBar.value = CalculateOverheat();
+    }
+
+    public void AddHeat(float heat)
+    {
+        CurrHeat += heat;
+        overheatBar.value = CalculateOverheat();
+        //if (CurrHeat >= 20f)
+        //{
+        //    Die();
+        //}
+    }
+
+    float CalculateOverheat()
+    {
+        return CurrHeat / MaxHeat;
+    }
+
+    void Die()
+    {
+        CurrHeat = 0f;
+        Debug.Log("overheated");
+    }
+}
